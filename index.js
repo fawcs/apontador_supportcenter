95abd9afff755cb2d5c7d03a2cc35e2c76a62710
Adriano S. Campanhola <ascampanhola@gmail.com>
2019-10-09 15:37:35 -0300

    Commit inicial
    Criacao da aplicacao para apontamentos no SC

---
+++ index.js
@@ -0,0 +1,45 @@
+const request = require("request");
+const csv = require('csv-parse');
+const fs = require('fs');
+const cfg = require('config');
+const results = [];
+
+const create_request = data => {
+  console.log(data);
+  return {
+    method: 'POST',
+    url: 'http://support-center.e-it.net/api/json/addTimeEntry',
+    headers: {
+      'cache-control': 'no-cache',
+      Connection: 'keep-alive',
+      Host: 'support-center.e-it.net',
+      'Cache-Control': 'no-cache',
+      Accept: '*/*',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+      apikey: cfg.api_key,
+      id: data[0],
+      supportRep: cfg.consultant,
+      billingStatus: 'billable',
+      description: data[2],
+      workHours: data[3],
+      executedTime: data[1] + ', 08:00:01',
+    }
+  };
+}
+
+fs.createReadStream('apontamentos.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () =>
+    results.forEach(data => {
+
+      if (data[0] == 'id') return;
+
+      request(create_request(data), (error, response, body) => {
+        if (error) console.log(new Error(error));
+        console.log(response);
+        console.log(body);
+      })
+    }));
